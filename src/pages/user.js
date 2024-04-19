82395f29b59c5b4a8c4f8323e50a5a4fdeeaae0a
Apoles <emine176gumus@gmail.com>
2024-04-19 12:58:18 +0300

    fix

--- src/pages/user.js
+++ src/pages/user.js
@@ -24,7 +24,7 @@ export default function Home({ isLoggedIn, user }) {
       return false;
     }
   };
-  const { data, error, isLoading } = useSWR(['https://recipes-theta-eight.vercel.app/loggedIn'], fetcher, {});
+  const { data, error, isLoading } = useSWR(['https://recipes-theta-eight.vercel.app/api/loggedIn'], fetcher, {});
 
   console.log(data);
 
